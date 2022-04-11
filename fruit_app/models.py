34b3bf1cb629c2bb065814495856e972d6b0e24d
xiaowang012 <1300202481@qq.com>
2022-04-11 09:27:50 +0800

    新增商品表，留言表，个人信息表，订单表，路由权限表，

--- fruit_app/models.py
+++ fruit_app/models.py
@@ -1,16 +1,59 @@
 #coding=utf-8
+from email.policy import default
 from django.db import models
 from django.db.models.base import Model
-from django.db.models.fields import CharField, IntegerField, TextField, TimeField,AutoField
+from django.db.models.fields import CharField, IntegerField, TextField,AutoField,DecimalField,BooleanField
+from django.db.models import DateTimeField
+from django.utils import timezone
 
-# Create your models here.
-#商品表表
+#商品表
 class Fruits(models.Model):
     id = AutoField(primary_key = True)
-    fruit_name = CharField(max_length=50)
-    book_type = CharField(max_length=50)
-    book_introduction = TextField(max_length=200)
-    issue_year = CharField(max_length=50)
-    number_of_downloads = IntegerField()
-    book_file_name = CharField(max_length=100)
-    add_book_time = CharField(max_length=100)
+    fruit_name = CharField(max_length = 50,default = '')
+    fruit_description = CharField(max_length = 200,default = '')
+    fruit_price = DecimalField(max_digits = 5,decimal_places = 2)
+    transportation_price = DecimalField(max_digits = 5,decimal_places = 2)
+    fruit_weight = CharField(max_length = 100,default = '')
+    Sales = IntegerField (default = 0)
+    fruit_pic_file_name = CharField(max_length = 200,default = '')
+    add_fruit_time = DateTimeField(default = timezone.now)
+
+#首页用户留言信息表
+class UserMessage(models.Model):
+    id = AutoField(primary_key = True)
+    commenting_user = CharField(max_length = 50,default = '')
+    commenting_content = CharField(max_length = 200,default = '')
+    anonymous = BooleanField()
+    commenting_time = DateTimeField(default = timezone.now)
+
+#个人信息表
+class UserInfo(models.Model):
+    id = AutoField(primary_key = True)
+    user_id = IntegerField()
+    chinese_name = CharField(max_length= 50 ,default = '')
+    sex = CharField(max_length = 10,default = '')
+    birthday = CharField(max_length= 50,default = '' )
+    constellation = CharField(max_length = 50 ,default = '')
+    phone_number = CharField(max_length = 50,default = '')
+    address1 = CharField(max_length = 200,default = '')
+    address2 = CharField(max_length = 200,default = '')
+    address3 = CharField(max_length = 200,default = '')
+    add_userinfo_time = DateTimeField(default = timezone.now)
+
+#订单表
+class FruitOrder(models.Model):
+    id = AutoField(primary_key = True)
+    customer =  CharField(max_length = 100,default = '')
+    order_number = CharField(max_length = 100,default = '')
+    money = DecimalField(max_digits = 8,decimal_places = 2)
+    add_order_time = DateTimeField(default = timezone.now)
+
+
+#路由权限表
+class RoutePermission(models.Model):
+    id = AutoField(primary_key = True)
+    group_name = CharField(max_length = 50,default = '')
+    url = CharField(max_length = 100,default = '')
+    description = CharField(max_length = 100,default = '')
+    add_route_permission_time = DateTimeField(default = timezone.now)
+
